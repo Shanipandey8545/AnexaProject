fc0292726671aef1b762c916451e559d96760beb
Manish723504 <2001manishmishra@gmail.com>
2026-09-04 01:30:16 +0530

    Add gunicorn and whitenoise for render deployment

--- AnexaProject/AnexaProject/settings.py
+++ AnexaProject/AnexaProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Yeh line add karein
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'AnexaProject.urls'
+AUTH_USER_MODEL = 'main_app.User'
 
 TEMPLATES = [
     {
@@ -118,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- AnexaProject/main_app/models.py
+++ AnexaProject/main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+
+class User(AbstractUser):
+    ROLE_TYPE = [
+        ('SuperAdmin','SuperAdmin'),
+        ('Admin','Admin'),
+        ('Customer','Customer'),
+    ]
+    
+    role = models.CharField(max_length=50, choices=ROLE_TYPE, default='Admin')
+    phone = models.CharField(max_length=15,null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    profile_image = models.FileField(upload_to='user_profile', null=True, blank=True)
+    license_number = models.CharField(max_length=100, null=True, blank=True)
+    aadhar_number = models.CharField(max_length=20, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
